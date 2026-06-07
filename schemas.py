a7dc17581951a14b3f60999b2ed7eb6a03cb548a
blueberry-unicorn <75934194+blueberry-unicorn@users.noreply.github.com>
2026-06-07 18:19:49 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,16 +1,22 @@
 from pydantic import BaseModel, EmailStr
 
+
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
-    password: str  # Make sure to hash this later
+    password: str
+
 
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
 
+
 class WebsiteCreate(BaseModel):
     website: str
+    user_id: int
+
 
 class FocusCreate(BaseModel):
-    status: str  # For example, "ACTIVE"
+    status: str
+    user_id: int
